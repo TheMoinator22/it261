35744d62a2c991e8c97f7b09aae32c5894bb44e5
TheMoinator22 <122125381+TheMoinator22@users.noreply.github.com>
2023-01-29 22:13:44 -0800

    small fix

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             <ul>
 
                 <li>
-                    <a href="">
+                    <a href="website/daily.php">
                         Switch
                     </a>
                 </li>
