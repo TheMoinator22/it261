ef6357e1a6a31b624145245f9dc6a25c320388f6
TheMoinator22 <122125381+TheMoinator22@users.noreply.github.com>
2023-02-27 16:59:02 -0800

    Update contact.php

--- website/contact.php
+++ website/contact.php
@@ -101,15 +101,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 include('./includes/header.php')
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="../weeks/week6/css/styles.css" type="text/css" rel="stylesheet">
-</head>
+
+
 <body>
 
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
