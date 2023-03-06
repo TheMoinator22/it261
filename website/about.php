a4f67721705e0da68fd3e2483edc5001ea3b97da
TheMoinator22 <122125381+TheMoinator22@users.noreply.github.com>
2023-03-05 17:20:25 -0800

    hw

--- website/about.php
+++ website/about.php
@@ -2,7 +2,9 @@
 include('./includes/header.php');
 ?>
 
-<img src="./images/sanrio_table_database.png" alt="photo of my sanrio table">
+<main>
+    <img src="./images/sanrio_table_database.png" alt="photo of my sanrio table">
+</main>
 
 <?php
 include('./includes/footer.php');
