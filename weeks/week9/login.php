200c23e6189bf09f233be6806c6684eb2a80fe19
TheMoinator22 <122125381+TheMoinator22@users.noreply.github.com>
2023-03-13 11:32:50 -0700

    Create login.php

---
+++ weeks/week9/login.php
@@ -0,0 +1,19 @@
+<?php
+include('server.php');
+// include(./includes/header.php);
+?>
+
+<h1>Login Page</h1>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+    <fieldset>
+        <label>Username</label>
+        <input type="text" name="username" value="<?php if(isset($_POST['username'])) echo htmlspecialchars($_POST['username']); ?>">
+
+        <label>Password</label>
+        <input type="password" name="password">
+
+        <button type="submit" class="btn" name="login_user">Login</button>
+
+        <button type="button" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>'">Reset</button>
+    </fieldset>
+</form>
