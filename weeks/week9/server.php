a6dc2c8f370ffc205aa0e969df4bec190cd12a7b
TheMoinator22 <122125381+TheMoinator22@users.noreply.github.com>
2023-03-13 11:40:40 -0700

    Update server.php

--- weeks/week9/server.php
+++ weeks/week9/server.php
@@ -6,7 +6,7 @@ session_start();
 include('config.php');
 // will eventually have header include here
 // include(./includes/header.php);
-$errors = '';
+// $errors = '';
 
 // this page will commincate with the database
 $iConn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die(myError(__FILE__,__LINE__,mysqli_connect_error()));
@@ -21,31 +21,31 @@ if (isset($_POST['reg_user'])) {
     $password_2 = mysqli_real_escape_string($iConn, $_POST['password_2']);
 
     if(empty($first_name)) {
-        array_push($errors, 'First Name is required');
+        array_push($errors, "First Name is required");
     } // end first name
 
     if(empty($last_name)) {
-        array_push($errors, 'Last Name is required');
+        array_push($errors, "Last Name is required");
     } // end last name
 
     if(empty($email)) {
-        array_push($errors, 'Email is required');
+        array_push($errors, "Email is required");
     } // end email
 
     if(empty($username)) {
-        array_push($errors, 'Username is required');
+        array_push($errors, "Username is required");
     } // end username
 
     if(empty($password_1)) {
-        array_push($errors, 'Password is required');
+        array_push($errors, "Password is required");
     } // end password
 
     if(empty($password_2)) {
-        array_push($errors, 'Your retyped password is required');
+        array_push($errors, "Your retyped password is required");
     } // end password
 
     if($password_1 !== $password_2) {
-        array_push($errors, 'Passwords are not matching');
+        array_push($errors, "Passwords are not matching");
     } // end password match
 
     // we are checking username and password and selecting it from the table
@@ -57,11 +57,11 @@ if (isset($_POST['reg_user'])) {
 
         // we cant have duplicate usernames or emails
         if($rows['username'] == $username) {
-            array_push($errors, 'Username already exists');
+            array_push($errors, "Username already exists");
         } // end username == username
 
         if($rows['email'] == $email) {
-            array_push($errors, 'Email already taken');
+            array_push($errors, "Email already taken");
         } // end email == email
 
     } // end if rows
