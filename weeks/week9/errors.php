331abef1551fd4a649de5d38ef0c1873cb097926
TheMoinator22 <122125381+TheMoinator22@users.noreply.github.com>
2023-03-13 16:44:05 -0700

    Update errors.php

--- weeks/week9/errors.php
+++ weeks/week9/errors.php
@@ -4,7 +4,7 @@ if(count($errors) > 0) :?>
 
     <div class="error">
         <?php foreach($errors as $error) :?>
-            <p><?php $error ?></p>
+            <?php echo $error ?>
         <?php endforeach ?>
     </div>
     <!-- end error -->
